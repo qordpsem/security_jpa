989669190ec14f9a7a34712bb8e0dcd18eb05f5a
qordpsem <152257506+qordpsem@users.noreply.github.com>
2024-09-24 00:11:22 +0900

    Revert "[Test] Issue 자동 닫힘 테스트"

--- src/main/java/com/example/security_jpa/dao/MemberDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.security_jpa.dao;
-
-import com.example.security_jpa.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberDAO extends JpaRepository<Member, String> {
-}
